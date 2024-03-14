d89a260b8a39f11384891b971747913ca130eeb0
Aakash Tripathi <aakash.tripathi0304@gmail.com>
2024-03-14 12:44:45 -0400

    RUST hello_world

---
+++ Rust/hello_world/hello_cargo/target/debug/deps/hello_cargo-631a5df1ed45a795.d
@@ -0,0 +1,5 @@
+/Users/aakash/Projects/JARVIS/Rust/hello_world/hello_cargo/target/debug/deps/hello_cargo-631a5df1ed45a795: src/main.rs
+
+/Users/aakash/Projects/JARVIS/Rust/hello_world/hello_cargo/target/debug/deps/hello_cargo-631a5df1ed45a795.d: src/main.rs
+
+src/main.rs:
---
+++ Rust/hello_world/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/Users/aakash/Projects/JARVIS/Rust/hello_world/hello_cargo/target/debug/hello_cargo: /Users/aakash/Projects/JARVIS/Rust/hello_world/hello_cargo/src/main.rs
